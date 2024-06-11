24109cac4538898a53c040d0d9e1a9f69cf4c764
Anurag <dev.anuragkr@gmail.com>
2024-06-12 00:45:04 +0530

    removed unused div

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,9 @@ import Nav from './Nav';
 
 function Header() {
   return (
-    <header>
-      <div className='py-4 flex justify-between items-center container'>
-        <img src='./Logo.svg' alt='Brand Logo' />
-        <Nav />
-      </div>
+    <header className='py-4 flex justify-between items-center container'>
+      <img src='./Logo.svg' alt='Brand Logo' />
+      <Nav />
     </header>
   );
 }
